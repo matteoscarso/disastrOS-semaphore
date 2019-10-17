cf8082560ab2ee1e4c1c5aef1eae192c66bacfbe
matteoscarso <scarso.1749415@studenti.uniroma1.it>
2019-10-17 12:24:00 +0200

    first_test

--- disastrOS_test.c
+++ disastrOS_test.c
@@ -18,11 +18,15 @@ void childFunction(void* args){
   printf("I will iterate a bit, before terminating\n");
   int type=0;
   int mode=0;
-  //sem1=disastrOS_semopen(1,1);
+  sem1=disastrOS_semOpen(1,1);
   int fd=disastrOS_openResource(disastrOS_getpid(),type,mode);
   printf("fd=%d\n", fd);
   printf("PID: %d, terminating\n", disastrOS_getpid());
-
+  
+  disastrOS_semWait(sem1);
+  disastrOS_semPost(sem1);
+  disastrOS_semClose(sem1);
+  
   for (int i=0; i<(disastrOS_getpid()+1); ++i){
     printf("PID: %d, iterate %d\n", disastrOS_getpid(), i);
     disastrOS_sleep((20-disastrOS_getpid())*5);
