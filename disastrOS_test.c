a802de7d448eedc251c6769574f667af62a4bdd7
matteoscarso <scarso.1749415@studenti.uniroma1.it>
2019-10-17 14:50:21 +0200

    test

--- disastrOS_test.c
+++ disastrOS_test.c
@@ -11,7 +11,7 @@ int sem_empty, sem_filled, sem_write, sem_read;
 int buf[BUF_LENGTH];
 int buf_length=BUF_LENGTH;
 int wr_idx=0;
-int rd_idx=0
+int rd_idx=0;
 
 unsigned long int shared_variable;
 
@@ -21,7 +21,7 @@ int producer(){
 	
 	int ret=shared_variable;
 	buf[wr_idx]=shared_variable;
-	wr_idx=(write_index+1) % buf_length;
+	wr_idx=(wr_idx+1) % buf_length;
 	shared_variable++;
 	
 	disastrOS_semPost(sem_write);
@@ -37,7 +37,7 @@ int consumer(){
 	disastrOS_semWait(sem_read);
 	
 	ret=buf[rd_idx];
-	rd_idx=(read_index+1)%buf_length;
+	rd_idx=(rd_idx+1)%buf_length;
 	
 	disastrOS_semPost(sem_empty);
 	disastrOS_semPost(sem_read);
@@ -56,16 +56,31 @@ void sleeperFunction(void* args){
 
 void childFunction(void* args){
   printf("Hello, I am the child function %d\n",disastrOS_getpid());
-  
+  printf("Apertura semafori\n");
   sem_filled=disastrOS_semOpen(1,0);
   sem_empty=disastrOS_semOpen(2, buf_length);
   sem_read=disastrOS_semOpen(3,1);
   sem_write=disastrOS_semOpen(4,1);
   
+  for(int i=0; i < COUNTER; i++){
+    if (disastrOS_getpid() % 2 == 0){
+     int sv = producer();
+     printf("THREAD %d: inserisco nel buffer il valore %d\n", disastrOS_getpid(), sv);
+     }
+    else {
+     int ret = consumer();
+     printf("THREAD %d: leggo dal buffer il valore %d\n", disastrOS_getpid(), ret);
+    }
+  }
   
   
   printf("PID: %d, terminating\n", disastrOS_getpid());
-
+  
+  printf("Chiusura semafori\n");
+  disastrOS_semClose(sem_filled);
+  disastrOS_semClose(sem_empty);
+  disastrOS_semClose(sem_write);
+  disastrOS_semClose(sem_read);
   
   disastrOS_exit(disastrOS_getpid());
 }
@@ -74,18 +89,19 @@ void childFunction(void* args){
 void initFunction(void* args) {
   disastrOS_printStatus();
   printf("hello, I am init and I just started\n");
-  disastrOS_spawn(sleeperFunction, 0);
   
+  
+   printf("STATO INIZIALE DEL BUFFER\n");
+  for(int i=0; i < buf_length; i++){
+    printf("%d ", buf[i]);
+  }
+  printf("\n");
+  
+  shared_variable=1;
 
   printf("I feel like to spawn 10 nice threads\n");
   int alive_children=0;
   for (int i=0; i<10; ++i) {
-    int type=0;
-    int mode=DSOS_CREATE;
-    printf("mode: %d\n", mode);
-    printf("opening resource (and creating if necessary)\n");
-    int fd=disastrOS_openResource(i,type,mode);
-    printf("fd=%d\n", fd);
     disastrOS_spawn(childFunction, 0);
     alive_children++;
   }
@@ -99,6 +115,13 @@ void initFunction(void* args) {
 	   pid, retval, alive_children);
     --alive_children;
   }
+  
+  printf("STATO FINALE DEL BUFFER\n");
+  for(int i=0; i < buf_length; i++){
+    printf("%d ", buf[i]);
+  }
+  printf("\n");
+  
   printf("shutdown!");
   disastrOS_shutdown();
 }
