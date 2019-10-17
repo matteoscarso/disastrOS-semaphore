1595b8ffd9b231b12f83e37e4f0ffbd6758579a5
matteoscarso <scarso.1749415@studenti.uniroma1.it>
2019-10-17 12:29:36 +0200

    Modifiche

--- disastrOS_sempost.c
+++ disastrOS_sempost.c
@@ -13,4 +13,13 @@ void internal_semPost(){
   
   Semaphore* sem = desc->semaphore;		//prendo il semaforo del descrittore	
   sem->count++;
+  
+  if(s->count <= 0){
+  	List_insert(&ready_list, ready_list.last, (ListItem*) running);
+  	SemDescriptorPtr* desc_ptr= (SemDescriptorPtr*)List_detach(&sem->waiting_descriptors, (ListItem*) sem->waiting_descriptors.first);
+  	List_insert(&sem->descriptors, s->descriptors.last, (ListItem*) desc_ptr);
+  	List_detach(&waiting_list, (ListItem*)desc_ptr->descriptor->pcb);
+  }
+  running->syscall_retvalue=0;
+  return;
 }
